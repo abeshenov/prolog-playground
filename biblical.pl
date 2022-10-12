15cb594082d613c587501232260644c189aa1841
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-10-12 04:11:09 -0500

    %% -> %

--- biblical.pl
+++ biblical.pl
@@ -1,37 +1,37 @@
-%% Biblical genealogy.
-%% The Art of Prolog, Chapter 1-2.
+% Biblical genealogy.
+% The Art of Prolog, Chapter 1-2.
 
-%% Genesis 11.
+% Genesis 11.
 
-%% And Terah lived seventy years, and begat Abram, Nahor, and Haran.
+% And Terah lived seventy years, and begat Abram, Nahor, and Haran.
 
 parent(terah,abram).
 parent(terah,nahor).
 parent(terah,haran).
 
-%% Now these are the generations of Terah:
-%% Terah begat Abram, Nahor, and Haran; and Haran begat Lot.
+% Now these are the generations of Terah:
+% Terah begat Abram, Nahor, and Haran; and Haran begat Lot.
 
 parent(haran,lot).
 
-%% And Abram and Nahor took them wives: the name of Abram's wife was Sarai;
-%% and the name of Nahor's wife, Milcah, the daughter of Haran,
-%% the father of Milcah, and the father of Iscah.
+% And Abram and Nahor took them wives: the name of Abram's wife was Sarai;
+% and the name of Nahor's wife, Milcah, the daughter of Haran,
+% the father of Milcah, and the father of Iscah.
 
 parent(haran,milcah).
 parent(haran,iscah).
 
-%% Genesis 21.
+% Genesis 21.
 
-%% And the Lord visited Sarah as he had said, and the Lord did unto Sarah as
-%% he had spoken. For Sarah conceived, and bare Abraham a son in his old age,
-%% at the set time of which God had spoken to him. And Abraham called the name
-%% of his son that was born unto him, whom Sarah bare to him, Isaac.
+% And the Lord visited Sarah as he had said, and the Lord did unto Sarah as
+% he had spoken. For Sarah conceived, and bare Abraham a son in his old age,
+% at the set time of which God had spoken to him. And Abraham called the name
+% of his son that was born unto him, whom Sarah bare to him, Isaac.
 
 parent(abram,isaac).
 parent(sarai,isaac).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 male(terah).
 male(abram).
@@ -44,10 +44,10 @@ female(sarai).
 female(milcah).
 female(iscah).
 
-%% UNIVERSAL FACT: "everyone likes pomegranates"
+% UNIVERSAL FACT: "everyone likes pomegranates"
 likes(_, pomegranates).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 father(X,Y) :- parent(X,Y), male(X).
 father(X) :- father(X,_).
@@ -69,7 +69,7 @@ brother(X,Y) :- sibling(X,Y), male(X).
 ancestor(Ancestor,Descendant) :- parent(Ancestor,Descendant).
 ancestor(Ancestor,Descendant) :- parent(Ancestor,Person), ancestor(Person,Descendant).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 /*
 
